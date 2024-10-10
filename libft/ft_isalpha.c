6646a464c27da4d2d37540a629ca1b442ae8153d
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2024-10-10 14:43:37 +0100

    Create ft_isalpha.c

---
+++ libft/ft_isalpha.c
@@ -0,0 +1,35 @@
+//#include <stdio.h>
+
+int     ft_isalpha(char *string)
+{
+        int      i;
+        i = 0;
+
+        while (string[i] != '\0')
+        {
+        if ((string[i] >= 'a' && string[i] <= 'z')
+                || (string[i] >= 'A' && string[i] <= 'Z'))
+        {
+                i++;
+        }
+        else
+        {
+//              printf("is not alphabetic\n");
+                return 1;
+        }
+        }
+//      printf("is alphabetic\n");
+        return 0;
+}
+/*
+int     main(void)
+{
+        char    *string = "abcde";
+        char    *string2 = "123";
+        char    *string3 = "abc1def";
+
+        ft_isalpha(string);
+        ft_isalpha(string2);
+        ft_isalpha(string3);
+        return 0;
+}*/
