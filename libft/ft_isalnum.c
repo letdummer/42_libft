45ca22bd37e9ebd2a713f727a19a444cceffbd8b
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2024-10-10 16:20:09 +0100

    Create ft_isalnum.c

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int     ft_isalnum(int c)
+{
+                if ((c >= 'a' && c <= 'z')
+                        || (c >= 'A' && c <= 'Z')
+                        || (c >= '0' && c <= '9'))
+                {
+                        printf("alfanum\n");
+                        return (1);
+                }
+                else
+                {
+                        printf("nao alfanum\n");
+                        return (0);
+                }
+
+
+}
+
+int     main(void)
+{
+        char    ch1 = 'a';
+        char    ch2 = '3';
+        char    ch3 = '$';
+
+        ft_isalnum(ch1);
+        ft_isalnum(ch2);
+        ft_isalnum(ch3);
+        return (0);
+}
