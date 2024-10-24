76f3d9d0b459c609849f53cb4b6962f882fc857d
letdummer <dummerleticia@gmail.com>
2024-10-24 16:13:42 +0100

    add reader

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -1,17 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldummer- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/24 16:10:21 by ldummer-          #+#    #+#             */
+/*   Updated: 2024/10/24 16:10:22 by ldummer-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //#include "libft.h"
 
 #include <stdio.h>
-#include <strings.h>
+//#include <strings.h>
 
-void    bzero(void s[.n], site_t n);
+void    ft_bzero(void *s, size_t n);
 
 int main(void)
 {
-    
+    int size = 9;
+    int array[size];
+
+  //  bzero(array, sizeof(array));
+    ft_bzero(array, sizeof(array));
+
+    int i = 0;
+    while (i < size)
+    {
+        printf("%d ", array[i]);
+        i++;
+    }
+    printf("\n"); 
+    return (0);
 }
 
-void    bzero(void s[.n], site_t n)
+void    ft_bzero(void *s, size_t n)
 {
-
-
+    size_t i;
+    char *c = s;
+    i = 0;
+    while (i < n)
+    {
+        c[i] = '\0';
+        i++;
+    }
 }
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,39 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldummer- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/24 16:10:37 by ldummer-          #+#    #+#             */
-/*   Updated: 2024/10/24 16:10:38 by ldummer-         ###   ########.fr       */
+/*   Created: 2024/10/24 16:10:57 by ldummer-          #+#    #+#             */
+/*   Updated: 2024/10/24 16:10:58 by ldummer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+//#include "libft.h"
 
-int     ft_isascii(int c)
-{
-        if (c >= 0 && c <= 127)
-        {
-                return (1);
-        }
-        else
-        {
-                return(0);
+#include <stdio.h>
 
-        }
-
-}
-
-int     main(void)
-{
-        char    ch1 = 'a';
-        char    ch2 = 'é';
-        char    ch3 = 'ñ';
-
-        ft_isascii(ch1);
-        ft_isascii(ch2);
-        ft_isascii(ch3);
-        return (0);
-}
