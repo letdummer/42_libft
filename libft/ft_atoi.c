33de496b2e3c59b2e7aa7a75ec53bde56f3cf6f1
letdummer <dummerleticia@gmail.com>
2024-11-04 15:55:56 +0000

    fix makefile

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,19 +1,25 @@
 #include "libft.h"
 
+#include <stdio.h>
+
 static const char	*ft_is_space(const char *str);
 static const char	*ft_is_signal(const char *str, int *signal);
-/*
-int					ft_atoi(const char *str);
- int	main(void)
-{
-	char	*string;
-	int		result;
 
-	string = "   -123a456";
-	result = ft_atoi(string);
-	printf("%i\n", result);
+int					ft_atoi(const char *str);
+int	main(void)
+{
+	
+	printf("ORIGINAL: atoi: %d\n", atoi("-999999999999"));
+	printf("CREATED: atoi: %d\n", ft_atoi("-999999999999"));
+	printf("\n");
+	printf("ORIGINAL: atoi: %d\n", atoi("-2147483648"));
+	printf("CREATED: atoi: %d\n", ft_atoi("-2147483648"));
+	printf("\n");
+	printf("ORIGINAL: atoi: %d\n", atoi("2147483647"));
+	printf("CREATED: atoi: %d\n", ft_atoi("2147483647"));
+
 	return (0);
-} */
+}
 
 int	ft_atoi(const char *str)
 {
