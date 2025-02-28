db67939444c7be3f217622112ecbf352ea277425
Leticia Dummer <ldummer-@student.42porto.com>
2025-02-28 19:39:03 +0000

    added functions to extra

---
+++ libft/ft_numlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_numlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldummer- <ldummer-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 19:33:03 by ldummer-          #+#    #+#             */
+/*   Updated: 2025/02/28 19:33:27 by ldummer-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//	Calculates the length of an integer
+
+int	ft_numlen(int n, int base)
+{
+	int	len;
+
+	len = 0;
+	if (!base)
+		base = 10;
+	if (n == 0)
+		return (1);
+	while (n)
+	{
+		++len;
+		n /= base;
+	}
+	return (len);
+}
---
+++ libft/ft_strcpy.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldummer- <ldummer-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/28 19:36:25 by ldummer-          #+#    #+#             */
+/*   Updated: 2025/02/28 19:37:42 by ldummer-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+// Copies size bytes from src to dest string
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	if (!dest || !src)
+		return (0);
+	if (!size)
+		return (ft_strlen(src));
+	if (size > 0)
+	{
+		i = 0;
+		while (*(src + i) && (i < (size - 1)))
+		{
+			*(dest + i) = *(src + i);
+			++i;
+		}
+		*(dest + i) = '\0';
+	}
+	return (ft_strlen(src));
+}
