76d6f34a4254b865e8eddbc86a2c50c82bb16dac
Letícia Dummer <62682118+letdummer@users.noreply.github.com>
2025-04-14 19:09:54 +0100

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -74,4 +74,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
+// EXTRA
+char	*get_next_line(int fd);
+
 #endif
